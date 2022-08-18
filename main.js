aeb7b5164ea2fd45897d43e5e7da160e1bea7c70
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-08-17 20:11:48 -0400

    Add javascript functionality

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("hello");
+console.log("js init");
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(b => {
+    b.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC': 
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                    break;
+                } catch {
+                    display.innerText = 'Error';
+                }
+            default: 
+                display.innerText += e.target.innerText;        
+        }
+    });
+});
